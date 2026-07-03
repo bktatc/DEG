7e5103e000f9f9e08dd40e022a454c2b8290c426
M. R. Bakhtiari <33508524+bktatc@users.noreply.github.com>
2026-07-03 09:57:29 +0330

    Update main.cpp
    Write proper comments for clarity

--- main.cpp
+++ main.cpp
@@ -8,16 +8,23 @@ int main()
     GeoUtils geo;
     Position p;
 
+    // variables to store data given by cin
     double latH, latM, latS;
     double lonH, lonM, lonS;
-
+    
+    // prompt user to enter data
     cout << "Enter latitude H,M,S: " << endl;
+    
+    // take input using cin
     cin >> latH >> latM >> latS;
 
     cout << "Enter lonitude H,M,S: " << endl;
     cin >> lonH >> lonM >> lonS;
-
+    
+    // calculate decimal degrees
     p = geo.ConvertHMStoDD(latH,latM,latS,lonH,lonM,lonS);
+    
+    // print the converted data
     cout << "The coordinate in decimal degrees: " << p.lat << ", " << p.lon << endl;
 
     return 0;
