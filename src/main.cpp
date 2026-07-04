d221e0a6d9eea31f332cafa3a1e7563cb82b2c18
mohamadreza-bakhtiari <bkt_atc@yahoo.com>
2026-07-04 10:29:32 +0330

    change variable name and prompt

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,20 @@ int main()
     GeoPoint p;
 
     // variables to store data given by cin
-    double latH, latM, latS;
-    double lonH, lonM, lonS;
+    double latD, latM, latS;
+    double lonD, lonM, lonS;
     
     // prompt user to enter data
-    cout << "Enter latitude H,M,S: " << endl;
+    cout << "Enter latitude D,M,S: " << endl;
     
     // take input using cin
-    cin >> latH >> latM >> latS;
+    cin >> latD >> latM >> latS;
 
-    cout << "Enter lonitude H,M,S: " << endl;
-    cin >> lonH >> lonM >> lonS;
+    cout << "Enter lonitude D,M,S: " << endl;
+    cin >> lonD >> lonM >> lonS;
     
     // calculate decimal degrees
-    p = geo.ConvertHMStoDD(latH,latM,latS,lonH,lonM,lonS);
+    p = geo.ConvertHMStoDD(latD,latM,latS,lonD,lonM,lonS);
     
     // print the converted data
     cout << "The coordinate in decimal degrees: " << p.lat << ", " << p.lon << endl;
